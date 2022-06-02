59bf228a9773bde6455ebdfa87c3dfe319729fb2
norahpack <norahpack14@gmail.com>
2022-06-02 15:31:19 -0700

    step 5 updates

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -2,11 +2,15 @@ package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
@@ -18,8 +22,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+    ImageView ivPoster;
 
-    @Override
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
@@ -27,6 +32,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster=(ImageView) findViewById(R.id.ivPoster);
 
 
         //retrieve, unwrap, and assign field from onCreate; unwrap the movie passed in via intent, using its simple name as a key.
@@ -39,6 +45,12 @@ public class MovieDetailsActivity extends AppCompatActivity {
         //converting vote average to a 0-5 star rating
         float voteAverage=movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage/ 2.0f);
+
+        String imageUrl;
+        int placeholderUrl;
+        placeholderUrl=R.drawable.flicks_backdrop_placeholder;
+
+        Glide.with(this).load(movie.getBackdropPath()).placeholder(placeholderUrl).into(ivPoster);
 
     }
 }
