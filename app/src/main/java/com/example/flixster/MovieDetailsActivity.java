3b9caece8c11574bcdbb7599f9db0ac46322fc8f
norahpack <norahpack14@gmail.com>
2022-06-03 15:44:02 -0700

    Final

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -39,52 +40,60 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
-        ivPoster=(ImageView) findViewById(R.id.ivPoster);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
 
 
         //retrieve, unwrap, and assign field from onCreate; unwrap the movie passed in via intent, using its simple name as a key.
-        movie=(Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
 
         //converting vote average to a 0-5 star rating
-        float voteAverage=movie.getVoteAverage().floatValue();
-        rbVoteAverage.setRating(voteAverage/ 2.0f);
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
 
         String imageUrl;
         int placeholderUrl;
-        placeholderUrl=R.drawable.flicks_backdrop_placeholder;
+        placeholderUrl = R.drawable.flicks_backdrop_placeholder;
 
         Glide.with(this).load(movie.getBackdropPath()).placeholder(placeholderUrl).into(ivPoster);
 
-
-
-    }
-
-    public void onClick(){
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(movie.getId(), new JsonHttpResponseHandler() {
+        ivPoster.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                try {
-                    JSONArray results = json.jsonObject.getJSONArray("results");
-                    if (results.length()==0){
-                        return;
-                    } else {
-                        String videoKey = results.getJSONObject(0).getString("key");
-                        //start up movie trailer activity page
+            public void onClick(View v) {
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(movie.getId(), new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        try {
+                            JSONArray results = json.jsonObject.getJSONArray("results");
+                            if (results.length() == 0) {
+                                return;
+                            } else {
+                                String videoKey = results.getJSONObject(0).getString("key");
+                                Intent intent = new Intent(MovieDetailsActivity.this, MovieTrailerActivity.class);
+                                intent.putExtra("videoKey", Parcels.wrap(videoKey));
+                                MovieDetailsActivity.this.startActivity(intent);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+                    }
+                });
+
+
             }
 
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
 
-            }
         });
     }
+
+
 }
--- app/src/main/java/com/example/flixster/MovieTrailerActivity.java
+++ app/src/main/java/com/example/flixster/MovieTrailerActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.flixster.models.Movie;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.parceler.Parcels;
 
 public class MovieTrailerActivity extends YouTubeBaseActivity {
 
@@ -17,19 +20,23 @@ public class MovieTrailerActivity extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_trailer);
 
+
+
         // temporary test video id -- TODO replace with movie trailer video id
-        final String videoId = "tKodtNFpzBA";
+        final String videoId =  (String) Parcels.unwrap(getIntent().getParcelableExtra("videoKey"));
 
         // resolve the player view from the layout
         YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
 
         // initialize with API key stored in secrets.xml
-        playerView.initialize("AIzaSyAcWdhMdI_r_FKcQ_T4Fp3viBGrb4y7h_w", new YouTubePlayer.OnInitializedListener() {
+        playerView.initialize("AIzaSyA5GHARyE7L8_9AkOfYeJ6dfb-3TEq3bdE", new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                                 YouTubePlayer youTubePlayer, boolean b) {
                 // do any work here to cue video, play video, etc.
+
                 youTubePlayer.cueVideo(videoId);
+                youTubePlayer.play();
             }
 
             @Override
